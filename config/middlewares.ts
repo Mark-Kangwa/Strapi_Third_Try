4601c16c55a56de55c367f315094b830e3343540
Mark Kangwa <168870459+Mark-Kangwa@users.noreply.github.com>
2025-12-02 14:17:32 +0200

    updated file

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,16 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173', 'http://localhost:8080', 'https://https://lovable.dev/projects/7ed7aa52-fe34-4551-9f8e-9127a561b28b'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
