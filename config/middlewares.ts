65c9da0da5157b6303892857589cf1f77861cc54
Mark Kangwa <168870459+Mark-Kangwa@users.noreply.github.com>
2025-12-02 14:43:35 +0200

    updated file2

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,7 +5,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'http://localhost:8080', 'https://https://lovable.dev/projects/7ed7aa52-fe34-4551-9f8e-9127a561b28b'],
+      origin: ['http://localhost:1337', 'http://localhost:8080', 'https://https://lovable.dev/projects/7ed7aa52-fe34-4551-9f8e-9127a561b28b'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
